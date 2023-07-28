ee27b31dad56f99677b8c8fb9a0aa869ba07fb53
Ankit Bansal <ankit.bansal1@bp.com>
2023-07-28 18:30:51 +0100

    set configuration changes

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
-    install_requires=[
-        #example-pkg-ankit-bansal==0.0.1
-    ]
+setup(
+    py_modules=['aws_moto_wrapper.wrapper'],
+    entry_points={
+        'pytest11': [
+            'moto-wrapper-plugin = aws_moto_wrapper.wrapper',
+        ],
+    },
 )
