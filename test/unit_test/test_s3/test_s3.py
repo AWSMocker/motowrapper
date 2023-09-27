1a99da43c90af492afe628b7bd2b3cde894ded2c
Ankit Bansal <ankit.bansal1@bp.com>
2023-09-27 12:09:06 +0100

    updated yaml files

--- test/unit_test/test_s3/test_s3.py
+++ test/unit_test/test_s3/test_s3.py
@@ -1,5 +1,3 @@
-# from moto_test.src.motowrapper.context import TestEnvContext
-# from moto_test.src.motowrapper.wrapper import AWSResourceInitializer
 import pytest
 
 from aws_lambda_powertools.logging import Logger
@@ -20,7 +18,7 @@ def test_moto_s3_mock_single_file(test_manager, s3_mock):
     __validate_s3_file_to_local(s3_mock, 'dip-single-file-bucket', 'mock/input/lookup.csv', 'test/input/lookup.csv')
 
 
-#
+
 def test_moto_s3_mock_multi_file(test_manager, s3_mock):
     res_out_validator = test_manager.mock_env('test/input/test_s3/test_s3.yaml')
     objects = s3_mock.list_objects_v2(Bucket='dip-sfg-integration-bucket')
